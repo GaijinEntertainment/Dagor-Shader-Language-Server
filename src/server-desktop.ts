d059cc7813abd3af6fae9bad08e4936021be60aa
Rácz Zalán <zalan_racz@gaijin.team>
2023-10-18 20:06:42 +0200

    Bundling the extension with Webpack and making it work on the web

---
+++ src/server-desktop.ts
@@ -0,0 +1,15 @@
+import {
+	Connection,
+	ProposedFeatures,
+	createConnection,
+} from 'vscode-languageserver/node';
+
+import { Server } from './server';
+
+export class ServerDesktop extends Server {
+	protected createConnection(): Connection {
+		return createConnection(ProposedFeatures.all);
+	}
+}
+
+new ServerDesktop();
--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,92 @@
 import {
 	CompletionItem,
 	CompletionItemKind,
+	Connection,
 	Diagnostic,
 	DiagnosticSeverity,
 	InitializeParams,
-	ProposedFeatures,
 	TextDocumentPositionParams,
 	TextDocumentSyncKind,
 	TextDocuments,
-	createConnection,
-} from 'vscode-languageserver/node';
-
+} from 'vscode-languageserver';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
-const connection = createConnection(ProposedFeatures.all);
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+export abstract class Server {
+	protected connection: Connection;
+	protected documents: TextDocuments<TextDocument>;
 
-connection.onInitialize((ip: InitializeParams) => {
-	return {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			completionProvider: {},
-		},
-	};
-});
-
-documents.onDidChangeContent((change) => {
-	validateTextDocument(change.document);
-});
-
-async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	const text = textDocument.getText();
-	const pattern = /\b[A-Z]{2,}\b/g;
-	let m: RegExpExecArray | null;
-
-	const diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text))) {
-		const diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length),
-			},
-			message: `${m[0]} is all uppercase.`,
-		};
-		diagnostics.push(diagnostic);
+	public constructor() {
+		this.connection = this.createConnection();
+		this.documents = new TextDocuments(TextDocument);
+		this.onInitialize();
+		this.addFeatures();
+		this.listen();
 	}
 
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+	protected abstract createConnection(): Connection;
+
+	private onInitialize(): void {
+		this.connection.onInitialize((ip: InitializeParams) => {
+			return {
+				capabilities: {
+					textDocumentSync: TextDocumentSyncKind.Incremental,
+					completionProvider: {},
+				},
+			};
+		});
+	}
+
+	protected addFeatures(): void {
+		this.addMockedCodeCompletion();
+		this.addMockedDiagnostics();
+	}
+
+	private addMockedCodeCompletion(): void {
+		this.connection.onCompletion(
+			(tdpp: TextDocumentPositionParams): CompletionItem[] => {
+				return [
+					{
+						label: 'Test code completion item 1',
+						kind: CompletionItemKind.Text,
+					},
+					{
+						label: 'Test code completion item 2',
+						kind: CompletionItemKind.Class,
+					},
+				];
+			}
+		);
+	}
+
+	private addMockedDiagnostics(): void {
+		this.documents.onDidChangeContent((change) => {
+			const textDocument = change.document;
+			const text = textDocument.getText();
+			const pattern = /\b[A-Z]{2,}\b/g;
+			let m: RegExpExecArray | null;
+
+			const diagnostics: Diagnostic[] = [];
+			while ((m = pattern.exec(text))) {
+				const diagnostic: Diagnostic = {
+					severity: DiagnosticSeverity.Warning,
+					range: {
+						start: textDocument.positionAt(m.index),
+						end: textDocument.positionAt(m.index + m[0].length),
+					},
+					message: `${m[0]} is all uppercase.`,
+				};
+				diagnostics.push(diagnostic);
+			}
+
+			this.connection.sendDiagnostics({
+				uri: textDocument.uri,
+				diagnostics,
+			});
+		});
+	}
+
+	private listen(): void {
+		this.documents.listen(this.connection);
+		this.connection.listen();
+	}
 }
-
-connection.onCompletion(
-	(tdpp: TextDocumentPositionParams): CompletionItem[] => {
-		return [
-			{
-				label: 'Test code completion item 1',
-				kind: CompletionItemKind.Text,
-			},
-			{
-				label: 'Test code completion item 2',
-				kind: CompletionItemKind.Class,
-			},
-		];
-	}
-);
-
-documents.listen(connection);
-connection.listen();
